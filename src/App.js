7c4a182f9a0fae3a630f427f6c91b8d3e0528215
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2022-07-21 02:06:32 +0300

    Delete unnecessary notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-//import Routes from '../src/routes/route';
 import Header from './components/Header/Header';
 import ListOfRequests from './components/ListOfRequests/ListOfRequests';
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
       <Header />
       <ListOfRequests />
     </div>
